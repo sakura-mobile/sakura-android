4b244f442a872f18767e5f7a55129b91e23c4571
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-15 16:45:04 +0300

    Use macro to form names for Java native methods.

--- src/androidgw.cpp
+++ src/androidgw.cpp
@@ -2,6 +2,8 @@
 
 #include "androidgw.h"
 
+#define JAVA_NATIVE_METHOD_NAME(class_name, method_name) Java_com_derevenetz_oleg_sakura_ ## class_name ## _ ## method_name
+
 AndroidGW *AndroidGW::Instance = nullptr;
 
 AndroidGW::AndroidGW(QObject *parent) : QObject(parent)
@@ -14,12 +16,12 @@ AndroidGW *AndroidGW::instance()
     return Instance;
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_sakura_SakuraActivity_bannerViewHeightUpdated(JNIEnv *, jclass, jint height)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(SakuraActivity, bannerViewHeightUpdated)(JNIEnv *, jclass, jint height)
 {
     emit AndroidGW::instance()->setBannerViewHeight(height);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_derevenetz_oleg_sakura_SakuraActivity_fbGameRequestCompleted(JNIEnv *, jclass, jint recipients_count)
+extern "C" JNIEXPORT void JNICALL JAVA_NATIVE_METHOD_NAME(SakuraActivity, fbGameRequestCompleted)(JNIEnv *, jclass, jint recipients_count)
 {
     emit AndroidGW::instance()->notifyGameRequestCompleted(recipients_count);
 }
